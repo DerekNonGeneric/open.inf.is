31b629febcfcf9bea648bbd2c0091c275db175f5
Derek Lewis <dereknongeneric@open.inf.is>
2024-10-26 21:32:37 -0400

    fix eleventy config

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,9 +1,9 @@
 import { EleventyI18nPlugin } from '@11ty/eleventy';
 
 export default async function (eleventyConfig) {
-  // Configure Eleventy
-  // Order matters, put this at the top of your configuration file.
-  eleventyConfig.setLayoutsDirectory('_layouts'); // relative to your input dir
+  // Configure Eleventy.
+  // Order matters, leave this at top of configuration file.
+  eleventyConfig.setLayoutsDirectory('_layouts'); // relative to input dir
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addGlobalData('siteTitle', 'OpenINF');
@@ -13,8 +13,8 @@ export default async function (eleventyConfig) {
   );
   eleventyConfig.addGlobalData('siteUrl', 'https://open.inf.is');
 
-  // Drafts, see also _data/eleventyDataSchema.js
-  eleventyConfig.addPreprocessor('drafts', '*', (data, content) => {
+  // Drafts, see also _data/eleventyDataSchema.js.
+  eleventyConfig.addPreprocessor('drafts', '*', (data /*, content*/) => {
     if (data.draft && process.env.ELEVENTY_ENV === 'production') {
       return false;
     }
@@ -32,10 +32,10 @@ export default async function (eleventyConfig) {
   });
 
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
-    // any valid BCP 47-compatible language tag is supported
-    defaultLanguage: 'en', // Required, this site uses "en"
+    // Any valid BCP 47-compatible language tag is supported.
+    defaultLanguage: 'en', // required
 
-    // When to throw errors for missing localized content files
+    // When to throw errors for missing localized content files.
     errorMode: 'strict', // throw an error if content is missing at /en/slug
   });
 }
